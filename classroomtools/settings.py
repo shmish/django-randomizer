360fa1b5a98a528e55c3df743416b97159c61bbf
Douglas Smith <dwsmith@vsb.bc.ca>
2018-09-03 20:18:16 -0700

    fix typos

--- classroomtools/settings.py
+++ classroomtools/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 from decouple import config
+import dj_database_url
 import configparser
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
--- classroomtools/wsgi.py
+++ classroomtools/wsgi.py
@@ -14,6 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "classroomtools.settings")
 
 
-pplication = get_wsgi_application()
+application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
 application = get_wsgi_application()
