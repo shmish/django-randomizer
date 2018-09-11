dff111629a9b12f4ec2ebe99a04fc424afe60592
Douglas Smith <dwsmith@vsb.bc.ca>
2018-09-11 16:44:59 -0700

    update wsgi

--- classroomtools/wsgi.py
+++ classroomtools/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "classroomtools.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "classroomtools.settings.production")
 
 application = get_wsgi_application()
